c9a25ed3670f029636bc672fdefd18f578857f30
Ubuntu <ubuntu@ip-172-31-35-99.ap-south-1.compute.internal>
2022-01-18 10:10:04 +0000

    exceptions handled

--- main.py
+++ main.py
@@ -31,68 +31,80 @@ class BaseRequestHandler(tornado.web.RequestHandler):
 class JupyterNotebookHandler(BaseRequestHandler):
     def post(self):
 
-        payload = json.loads(self.request.body)
-        project = payload["project"]
-        file = payload["notebook_name"]
-        domain = config("DOMAIN_NAME")
+        try:
+            payload = json.loads(self.request.body)
+            project = payload["project"]
+            file = payload["notebook_name"]
+            domain = config("DOMAIN_NAME")
 
-        notebooks_dir = "notebooks"
+            notebooks_dir = "notebooks"
 
-        # Check whether the specified path exists or not
-        isExist = os.path.exists(f"{notebooks_dir}/{project}/")
-        if not isExist:
-            # Create a new directory because it does not exist
-            os.mkdir(f"{notebooks_dir}/{project}/")
+            # Check whether the specified path exists or not
+            isExist = os.path.exists(f"{notebooks_dir}/{project}/")
+            if not isExist:
+                # Create a new directory because it does not exist
+                os.mkdir(f"{notebooks_dir}/{project}/")
 
-            # create a new notebook file
-            with open(f"{notebooks_dir}/{project}/{file}.ipynb", "w") as notebook:
-                pass
+                # create a new notebook file
+                with open(f"{notebooks_dir}/{project}/{file}.ipynb", "w") as notebook:
+                    pass
 
-            # convert normal file to jupyter notebook file
-            FCM().new(path=f"{notebooks_dir}/{project}/{file}.ipynb")
+                # convert normal file to jupyter notebook file
+                FCM().new(path=f"{notebooks_dir}/{project}/{file}.ipynb")
 
-            notebook_url = f"{domain}/{notebooks_dir}/{project}/{file}.ipynb"
-            self.write({"notebook_url": notebook_url})
-        else:
+                notebook_url = f"{domain}/{notebooks_dir}/{project}/{file}.ipynb"
+                self.write({"notebook_url": notebook_url})
+            else:
 
-            # create a new notebook file
-            with open(f"{notebooks_dir}/{project}/{file}.ipynb", "w") as notebook:
-                pass
+                # create a new notebook file
+                with open(f"{notebooks_dir}/{project}/{file}.ipynb", "w") as notebook:
+                    pass
 
-            # convert normal file to jupyter notebook file
-            FCM().new(path=f"{notebooks_dir}/{project}/{file}.ipynb")
+                # convert normal file to jupyter notebook file
+                FCM().new(path=f"{notebooks_dir}/{project}/{file}.ipynb")
 
-            notebook_url = f"{domain}/{notebooks_dir}/{project}/{file}.ipynb"
-            self.write({"notebook_url": notebook_url})
+                notebook_url = f"{domain}/{notebooks_dir}/{project}/{file}.ipynb"
+                self.write({"notebook_url": notebook_url})
+        except Exception as e:
+            self.write({"error": str(e)})
+
+
+
+
 
 
 class ProjectNotebookHandler(BaseRequestHandler):
     def get(self, id):
-        project_uid = id
-        notebooks_dir = "notebooks"
+        try:
+            project_uid = id
+            notebooks_dir = "notebooks"
 
-        notebooks = os.listdir(f"{notebooks_dir}/{project_uid}")
-        domain = config("DOMAIN_NAME")
+            notebooks = os.listdir(f"{notebooks_dir}/{project_uid}")
+            domain = config("DOMAIN_NAME")
 
-        notebooks_paths = [
-            {
-                "notebook_name": notebook,
-                "created": datetime.datetime.fromtimestamp(
-                    os.path.getctime(
-                        os.path.abspath(f"{notebooks_dir}/{project_uid}/{notebook}")
-                    )
-                ).strftime("%m/%d/%Y %H:%M %p"),
-                "updated": datetime.datetime.fromtimestamp(
-                    os.path.getmtime(
-                        os.path.abspath(f"{notebooks_dir}/{project_uid}/{notebook}")
-                    )
-                ).strftime("%m/%d/%Y %H:%M %p"),
-                "notebook_url": f"{domain}/{notebooks_dir}/{project_uid}/{notebook}",
-            }
-            for notebook in notebooks
-            if notebook.endswith(".ipynb")
-        ]
-        self.write({"notebooks": notebooks_paths})
+            notebooks_paths = [
+                {
+                    "notebook_name": notebook,
+                    "created": datetime.datetime.fromtimestamp(
+                        os.path.getctime(
+                            os.path.abspath(f"{notebooks_dir}/{project_uid}/{notebook}")
+                        )
+                    ).strftime("%m/%d/%Y %H:%M %p"),
+                    "updated": datetime.datetime.fromtimestamp(
+                        os.path.getmtime(
+                            os.path.abspath(f"{notebooks_dir}/{project_uid}/{notebook}")
+                        )
+                    ).strftime("%m/%d/%Y %H:%M %p"),
+                    "notebook_url": f"{domain}/{notebooks_dir}/{project_uid}/{notebook}",
+                }
+                for notebook in notebooks
+                if notebook.endswith(".ipynb")
+            ]
+            self.write({"notebooks": notebooks_paths})
+        except Exception as e:
+            self.write({"error": str(e)})
+
+
 
 
 def routes():
